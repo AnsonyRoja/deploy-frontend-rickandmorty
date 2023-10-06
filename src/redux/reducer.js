6c767dfb186ccb145e778edbb1d1c2301a9f8248
AnsonyRoja <ansonyrojas2015@gmail.com>
2023-10-06 08:12:06 -0400

    actualizacion

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,57 +1,64 @@
-import { GET_FAV, ADD_FAV, REMOVE_FAV, FILTER, ORDER } from "./actions-types";
+
+
+import { ADD_FAV, REMOVE_FAV, FILTER, ORDER } from "./actions-types";
 
 const initialState = {
+
   myFavorites: [],
   allCharacters: [],
-};
+
+}
 
 const reducer = (state = initialState, { type, payload }) => {
+
+
+
   switch (type) {
+
     case ADD_FAV:
+
       return {
         ...state,
         myFavorites: payload,
         allCharacters: payload,
-      };
+
+      }
 
     case REMOVE_FAV:
+
       return {
+
         ...state,
         myFavorites: payload,
         allCharacters: payload,
-      };
-
-    case GET_FAV:
-      return {
-        ...state,
-        myFavorites: payload,
-      };
+      }
 
     case FILTER:
-      const allCharactersFiltered =
-        payload === "allCharacters"
-          ? [...state.allCharacters]
-          : state.allCharacters.filter((character) => character.gender === payload);
+      const allCharactersFiltered = state.allCharacters.filter(character => character.gender === payload);
       return {
+
         ...state,
-        myFavorites: allCharactersFiltered.filter((character) =>
-          state.myFavorites.some((favorite) => favorite.id === character.id)
-        ),
-      };
+        myFavorites:
+          payload === 'allCharacters'
+            ? [...state.myFavorites]
+            : allCharactersFiltered,
+
+
+      }
 
     case ORDER:
       let allCharactersCopy = [...state.myFavorites];
       return {
         ...state,
         myFavorites:
-          payload === "A"
-            ? allCharactersCopy.sort((a, b) => a.id - b.id)
-            : allCharactersCopy.sort((a, b) => b.id - a.id),
-      };
+          payload === 'A' ? allCharactersCopy.sort((a, b) => a.id - b.id) :
+            allCharactersCopy.sort((a, b) => b.id - a.id),
+      }
 
     default:
       return { ...state };
   }
-};
+
+}
 
 export default reducer;
