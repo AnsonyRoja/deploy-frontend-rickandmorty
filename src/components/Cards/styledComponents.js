e886a209f9595582ffc488c06190c4f9506aabd5
AnsonyRoja <ansonyrojas2015@gmail.com>
2023-10-06 13:50:02 -0400

    commit

--- src/components/Cards/styledComponents.js
+++ src/components/Cards/styledComponents.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const CardsContainer = styled.div`
   display: flex;
   background-color: rgb(100, 50, 110);
-  overflow: auto;
+  overflow: auto; /* Cambia "auto" a "scroll" */
   flex-direction: row;
   justify-content: space-around;
   width: 90%;
@@ -12,10 +12,8 @@ export const CardsContainer = styled.div`
   flex-wrap: wrap;
   padding: 1em;
   border-radius: 15px;
-  box-shadow: 0 0 10px rgba(0, 0, 0, 0.2); /* Sombra alrededor del contenedor */
-  transition: transform 0.3s ease; /* Transición suave para transformaciones */
-
- 
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+  transition: transform 0.3s ease;
 
   @media screen and (max-width: 768px) {
     width: 100%;
