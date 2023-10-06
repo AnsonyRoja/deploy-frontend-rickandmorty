000f41e60b003dda08ffb98106a4daa7921dac5c
AnsonyRoja <ansonyrojas2015@gmail.com>
2023-10-05 20:59:05 -0400

    se agregaron emojis al sobre mi

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -10,17 +10,17 @@ const About = () => {
             <h1>Acerca de Mi</h1>
             <p>
 
-                <h2>¡Hola, soy Ansony Rojas!</h2>
+                <h2>¡Hola, soy Ansony Rojas! 😀</h2>
 
                 Soy un amante de la tecnología, y me gusta "Rick and Morty". este es un proyecto de prueba para practicar React y Redux con una API pública.
 
-                <h3>Un Poco Sobre Mí:</h3>
+                <h3>Un Poco Sobre Mí: ⌛</h3>
 
                 Programacion: me gusta aprender nuevas tecnologías y crear proyectos. el cual me dejan una amplia gama de conocimientos y experiencia.
 
-                <h3>Futbol:</h3> me gusta jugar y ver fútbol. Soy un gran fanático del Real Madrid.
+                <h3>Futbol: ⚽</h3> me gusta jugar y ver fútbol. Soy un gran fanático del Real Madrid.
 
-                <h3>Videojuegos:</h3> me encanta jugar videojuegos. me gusta jugar juegos de rol como league of legends, moba, etc.
+                <h3>Videojuegos: 🎮 </h3> me encanta jugar videojuegos. Me gusta jugar juegos de rol como league of legends, moba, etc.
 
 
 
