e8a97dd4832680f6dc00b428e4572fb002a87998
AnsonyRoja <ansonyrojas2015@gmail.com>
2023-10-06 15:24:41 -0400

    mensaje de ayuda al hacer click en ?

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -12,6 +12,8 @@ const Form = ({ login }) => {
         password: ''
 
     })
+    const [mostrarMensaje, setMostrarMensaje] = useState(false);
+
 
     const handledSubmit = (event) => {
 
@@ -36,19 +38,24 @@ const Form = ({ login }) => {
         }))
 
     }
+    const toggleMensaje = () => {
+
+        setMostrarMensaje(!mostrarMensaje);
+    }
 
     return (
 
         <form onSubmit={handledSubmit}>
 
             <label htmlFor="email" className={styles.email}>Email</label>
-            <input type="email" className={styles.inputEmail} name="email" value={userData.email} onChange={handleChange} />
+            <span onClick={toggleMensaje}>?</span><input type="email" className={styles.inputEmail} name="email" value={userData.email} onChange={handleChange} />
             <label htmlFor="password" className={styles.password}>Password</label>
             <input type="password" className={styles.inputPassword} name="password" value={userData.password} onChange={handleChange} />
 
             <div className={styles.divs}>
                 {errors.email && <p style={{ color: "white", fontWeight: 'bolder', width: '250px' }}>{errors.email}</p>}
                 {errors.password && <p style={{ color: "white", fontWeight: 'bolder', width: '250px' }}>{errors.password}</p>}
+                {mostrarMensaje && <p style={{ color: "white", fontWeight: 'bolder', width: '250px' }}><p>Correo: user@gmail.com</p><p>Contraseña: 123456789</p></p>}
             </div>
 
             <button className={styles.buttons}>Submit</button>
