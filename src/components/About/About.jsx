7d5463ad79584ec7be3cd96a746fb504f117c08a
AnsonyRoja <ansonyrojas2015@gmail.com>
2023-10-05 20:45:57 -0400

    Add to about me

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -3,14 +3,30 @@
 
 
 
-const About = () =>{
+const About = () => {
 
-    return(
-            <div>
-            <h1>Work in Progress...</h1>
-            <p>Ansony</p>
-         
-            </div>
+    return (
+        <div>
+            <h1>Acerca de Mi</h1>
+            <p>
+
+                ¡Hola, soy Ansony Rojas!
+
+                Soy un amante de la tecnología, y me gusta "Rick and Morty". este es un proyecto de prueba para practicar React y Redux con una API pública.
+
+                Un Poco Sobre Mí:
+
+                Programacion: me gusta aprender nuevas tecnologías y crear proyectos. el cual me dejan una amplia gama de conocimientos y experiencia.
+
+                Futbol: me gusta jugar y ver fútbol. Soy un gran fanático del Real Madrid.
+
+                Videojuegos: me encanta jugar videojuegos. Me gusta jugar juegos de rol como league of legends, moba, etc.
+
+
+
+            </p>
+
+        </div>
     );
 
 }
